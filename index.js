8eb27f8655a67b757cfade53321867c29f6c5044
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-02-17 21:00:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ function Scooter (year, color, model){
    this.color = color
    this.model = model
 }
-function Driver ()
+function Driver (name, age, experience){
+  this.name = name 
+  this.age = age 
+  this.experience = experience
+}
